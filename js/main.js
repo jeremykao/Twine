6e0c6023087434cd71a58ad2903fb2bb7ea27047
dmhartsook <dmhartsook@gmail.com>
2013-06-19 17:41:02 -0600

    Update main.js
    Added geolocation functions

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ $(document).ready(function(){
   var stepOneScreen = $('#step-one');
   var stepTwoScreen = $('#step-two');
   var stepThreeScreen = $('#step-three');
-
+  
+  var selfLat;
+  var selfLong;
+  
   stepTwoScreen.hide();
   stepThreeScreen.hide();
   $("#search-btn").click(function(){slideScreen(stepOneScreen, stepTwoScreen);});
@@ -18,3 +21,17 @@ var slideScreen = function(curr, next) {
   next.css("visibility", "visible");
   next.fadeIn();
 };
+
+// Find geolocation
+var getCoords = function() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(returnCoords);
+  }
+  else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+var returnCoords = function() {
+  selfLat = position.coords.latitude;
+  selfLong = position.coords.longitude;
+}
