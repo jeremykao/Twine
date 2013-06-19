9d07d97ace05463839811cc5424919f113e65148
dmhartsook <dmhartsook@gmail.com>
2013-06-19 12:24:06 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // Where UI-related JS and other general JS goes
 var Move = function() {
   $("#step-one").hide();
+  $("step-two").css("visibility", "visible");
   $("#step-two").fadeIn();
 };
 var Show3 = function() {
 	$("#step-two").hide();
-	$("#step-three").show();
+	$("#step-three").css("visibility", "visible");
+	$("#step-three").fadeIn();
 }
 
 var Main = function() {
