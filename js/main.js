30161e5bf5aefd88c3b3da55b311b081549e025b
earllee <earlvlee@gmail.com>
2013-06-19 16:47:10 -0700

    progress bar work

--- js/main.js
+++ js/main.js
@@ -5,9 +5,21 @@ $(document).ready(function(){
   var stepOneScreen = $('#step-one');
   var stepTwoScreen = $('#step-two');
   var stepThreeScreen = $('#step-three');
+  var progressBar = $('#progress-bar > .bar');
+  var progressBarContainer = $('#progress-bar-container');
 
-  $("#search-btn").click(function(){slideScreen(stepOneScreen, stepTwoScreen);});
-  $("#continue-btn").click(function(){slideScreen(stepTwoScreen, stepThreeScreen);});
+  stepTwoScreen.hide();
+  stepThreeScreen.hide();
+  $("#search-btn").click(function(){
+    slideScreen(stepOneScreen, stepTwoScreen);
+    progressBar.css('width', '20%');
+  });
+  $("#continue-btn").click(function(){
+    slideScreen(stepTwoScreen, stepThreeScreen);
+    progressBarContainer.removeClass('span6');
+    progressBar.addClass('span12');
+    progressBar.css('width', '70%');
+  });
 }); 
 
 // Feed in DOM elements to switch between two div screens
