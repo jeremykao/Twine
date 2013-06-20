b71b32a072f481ea3a25e0c489d577d9ddd259ab
dmhartsook <dmhartsook@gmail.com>
2013-06-20 14:53:31 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ function loadStepTwo(){
     slideScreen(stepOneScreen, stepTwoScreen);
     progressBar.css('width', '40%');
   if ($("#results-list").length == 1){
-    $("#results-list").append("<li>There were no results from your search</li><li></li>");
+    //$("#results-list").append("<li>There were no results from your search</li><li></li>");
     //$("#continue-btn").attr("disabled", true);
   }
 }
