74477cd30f5f4b4d2910a3006274482940d10466
earllee <earlvlee@gmail.com>
2013-07-11 12:58:40 -0500

    fixed tabs issue

--- main.py
+++ main.py
@@ -21,55 +21,55 @@ from google.appengine.api import mail
 from google.appengine.ext.webapp import template
 
 class MainHandler(webapp2.RequestHandler):
-	def get(self):
-		user = users.get_current_user()
-		showGmail = False
-		loginLink = ""
-		if user is None:
-			loginLink = users.create_login_url('/')
-			showGmail = True
-		template_values = {'showGmail': showGmail, 'loginLink': loginLink}
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, template_values))
+    def get(self):
+        user = users.get_current_user()
+        showGmail = False
+        loginLink = ""
+        if user is None:
+            loginLink = users.create_login_url('/')
+            showGmail = True
+        template_values = {'showGmail': showGmail, 'loginLink': loginLink}
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
 
 class TestEmailHandler(webapp2.RequestHandler):
-	def get(self):
-		template_values = {}
-		path = os.path.join(os.path.dirname(__file__), 'testEmail.html')
-		self.response.out.write(template.render(path, template_values))
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'testEmail.html')
+        self.response.out.write(template.render(path, template_values))
 
 class SendHandler(webapp2.RequestHandler):
-	def post(self):
-		user = users.get_current_user()
-		sendees = self.request.get("sendees")
-		sender = user.nickname() + "<" + user.email() + ">"
-		subject = self.request.get("subject")
-		message = self.request.get("message")
-		activityStr = ""
-		if (sendees[-1] == ','):
-			sendees = sendees[0:-1]
-		if ( sendees.find(",") != -1 ):
-			sendees = sendees.split(",")
-		else:
-			sendees = [sendees]
+    def post(self):
+        user = users.get_current_user()
+        sendees = self.request.get("sendees")
+        sender = user.nickname() + "<" + user.email() + ">"
+        subject = self.request.get("subject")
+        message = self.request.get("message")
+        activityStr = ""
+        if (sendees[-1] == ','):
+            sendees = sendees[0:-1]
+        if ( sendees.find(",") != -1 ):
+            sendees = sendees.split(",")
+        else:
+            sendees = [sendees]
 
-		for sendee in sendees:
-			activityStr += sendee + ", "
-			email = mail.EmailMessage(sender=sender, subject=subject, to=sendee, body=message)
-			email.send()
-		self.response.out.write(activityStr)
+        for sendee in sendees:
+            activityStr += sendee + ", "
+            email = mail.EmailMessage(sender=sender, subject=subject, to=sendee, body=message)
+            email.send()
+        self.response.out.write(activityStr)
 
 
 class TestFbEventHandler(webapp2.RequestHandler):
-	def get(self):
-		template_values = {}
-		path = os.path.join(os.path.dirname(__file__), 'testFbEvent.html')
-		self.response.out.write(template.render(path, template_values))
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'testFbEvent.html')
+        self.response.out.write(template.render(path, template_values))
 
 class LegalHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {}
-		path = os.path.join(os.path.dirname(__file__), 'legal.html')
+        path = os.path.join(os.path.dirname(__file__), 'legal.html')
         self.response.out.write(template.render(path, template_values))
 
 app = webapp2.WSGIApplication([
